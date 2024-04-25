6b927771ad1661c77744e46d8d3e8ce177fb33c7
Rana Wahid <ranawahid34@gmail.com>
2024-04-25 18:11:08 +0200

    add building mst graph function

--- graph.cs
+++ graph.cs
@@ -20,6 +20,15 @@ namespace Plagiarism_Validation
                 AddEdge(file2, file1, percentage2);  
             }
         }
+        public void BuildMSTGraph(List<(string file1, int percentage1, string file2, int percentage2)> pairs)
+        {
+            foreach (var (file1, percentage1, file2, percentage2) in pairs)
+            {
+                int max_percentage = Math.Max(percentage1, percentage2);
+                AddEdge(file1, file2, max_percentage);
+                AddEdge(file2, file1, max_percentage);
+            }
+        }
 
         private void AddEdge(string source, string destination, int weight)
         {
